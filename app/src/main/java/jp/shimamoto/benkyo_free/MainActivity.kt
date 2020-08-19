6373de7ffe4fd933bbd39fa73bfd803c7b6d6792
shimamoto-g1 <68539210+shimamoto-g1@users.noreply.github.com>
2020-08-19 11:43:58 +0900

    空のmutableListを使用した。 XXX[y]=z は空の状態からは使えないようだ

--- app/src/main/java/jp/shimamoto/benkyo_free/MainActivity.kt
+++ app/src/main/java/jp/shimamoto/benkyo_free/MainActivity.kt
@@ -1,5 +1,6 @@
 package jp.shimamoto.benkyo_free
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -9,6 +10,7 @@ import java.time.format.DateTimeFormatter
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SimpleDateFormat")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
